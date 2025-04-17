36e6910c9e0490bc80af664fc9c2188c0b24a497
@vishalnimbalkar <vishal.nimbalkar@geekrabit.com>
2025-04-18 00:53:27 +0530

    Joins ans assignment2

---
+++ SQL/Assignment2.sql
@@ -0,0 +1,103 @@
+create database assignment2;
+use assignment2;
+-- Create a table named employee with the following columns:
+-- EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER   | HIRE_DATE  | JOB_ID     | SALARY  | MANAGER_ID | DEPARTMENT_ID
+create table departments(
+	department_id int primary key auto_increment,
+    department_name varchar(50)
+);
+create table managers(
+	manager_id int primary key auto_increment,
+    manager_name varchar(50)
+);
+create table employee(
+	employee_id int primary key auto_increment,
+    fist_name varchar(50),
+    last_name varchar(50),
+    email varchar(50),
+    phone_number varchar(20),
+    hire_date date,
+    job_id varchar(50),
+    salary decimal,
+    manager_id int,
+    department_id int,
+    foreign key(manager_id) references managers(manager_id),
+    foreign key(department_id) references departments(department_id)
+);
+alter table employee rename column fist_name to first_name;
+INSERT INTO departments (department_name) VALUES 
+('HR'), ('IT'), ('Sales'), ('Finance'), ('Logistics'), ('Marketing'), ('Support');
+
+INSERT INTO managers (manager_name) VALUES 
+('Alice Smith'), ('Bob Johnson'), ('Carol White'), ('David Brown');
+
+INSERT INTO employee (first_name, last_name, email, phone_number, hire_date, job_id, salary, manager_id, department_id) VALUES 
+('John', 'Doe', 'john.doe@example.com', '1234567890', '2023-12-20', 'IT_PROG', 60000, 1, 2),
+('Jane', 'Smith', 'jane.smith@example.com', '1234567891', '2022-05-14', 'HR_REP', 45000, 1, 1),
+('Mike', 'Johnson', 'mike.j@example.com', '1234567892', '2024-03-12', 'SA_REP', 30000, 2, 3),
+('Anna', 'Williams', 'anna.w@example.com', '1234567893', '2023-11-25', 'IT_PROG', 52000, 2, 2),
+('Sara', 'Davis', 'sara.d@example.com', '1234567894', '2022-08-10', 'AC_ACCOUNT', 40000, 3, 4),
+('Tom', 'Brown', 'tom.b@example.com', '1234567895', '2025-01-01', 'SA_REP', 55000, 3, 3),
+('Emily', 'Taylor', 'emily.t@example.com', '1234567896', '2021-07-15', 'MK_REP', 50000, 4, 6),
+('Chris', 'Martin', 'chris.m@example.com', '1234567897', '2020-04-18', 'IT_PROG', 70000, 4, 2),
+('Nina', 'Patel', 'nina.p@example.com', '1234567898', '2023-06-05', 'HR_REP', 48000, 1, 1),
+('Robert', 'Lee', 'robert.l@example.com', '1234567899', '2023-09-01', 'AC_ACCOUNT', 41000, 2, 4),
+('David', 'Clark', 'david.c@example.com', '1234567800', '2023-10-10', 'SA_REP', 47000, 3, 3),
+('Sophia', 'Walker', 'sophia.w@example.com', '1234567801', '2022-12-05', 'MK_REP', 43000, 4, 6);
+
+select * from employee;
+select * from departments;
+select * from managers;
+
+-- Assignment Questions: -
+-- 	Q1. Write a query to list employees whose salary is greater than 40000 and belong to department 6.
+select e.* 
+from employee e
+join departments d
+on e.department_id = d.department_id
+where d.department_id = 6 and e.salary > 40000;
+
+-- 	Q2. Write a query to find departments where more than 2 employees are working.
+select e.department_id, count(e.department_id) emp_cnt
+from departments d 
+join employee e
+on e.department_id = d.department_id
+group by e.department_id
+having emp_cnt > 2;
+
+-- 	Q3. Write a query to sort employees by salary in descending order and show only top 3.
+select * from employee order by salary desc limit 3;
+
+-- 	Q4. Write a query to find employees whose job_id is not 'IT_PROG'.
+select * from employee where job_id != 'IT_PROG';
+
+-- 	Q5. Write a query to count the number of employees under each manager.
+select manager_id, count(manager_id) as emp_under_manger from employee group by manager_id;
+
+-- 	Q6. Write a query to list job roles having more than one employee.
+select job_id, count(job_id) as emp_cnt from employee group by job_id having emp_cnt > 1;
+
+-- 	Q7. Write a query to show the details of the most recently hired employee.
+select * from employee order by hire_date desc limit 1;
+
+select * from employee;
+-- 	Q8. Write a query to find the number of employees hired in each year.
+select year(hire_date) as yr, count(year(hire_date)) as yr_cnt from employee group by yr;
+
+-- 	Q9. Write a query to get job_ids where the total salary exceeds 45000.
+select job_id, sum(salary) as total from employee group by job_id having total > 45000;
+
+-- 	Q10. Write a query to list employees in alphabetical order of their last name.
+select * from employee order by last_name;
+
+-- 	Q11. Write a query to display the average salary of employees in each job_id.
+select job_id, avg(salary) from employee group by job_id;
+
+-- 	Q12. Write a query to list the lowest-paid employee in each department.
+select department_id, min(salary) as low from employee group by department_id;
+
+-- 	Q13. Write a query to find all employees hired before '2024-01-01' and earning more than 40000. 
+select * from employee where hire_date < '2024-01-01' and salary > 40000; 
+
+-- 	Q.14  Write a query to display employee details where salary is between 40000 and 60000.
+select * from employee where salary between 40000 and 60000;
--- SQL/Day6_17Apr_Joins.sql
+++ SQL/Day6_17Apr_Joins.sql
@@ -102,7 +102,168 @@ right join bookings
 on users.id = bookings.customer_id;
 
 -- 5.CROSS JOIN
+-- it returns every combination of row from table1 with every row from table2
+-- suppose:
+-- table A has 3 rows
+-- table B has 4 rows 
+-- result : 3 * 4 = 12 rows 
+
 select users.id, users.name, bookings.booking_status 
 from users
-cross join bookings
+cross join bookings;
+
+select  users.id, users.name, bookings.booking_status  from users, bookings;
+-- this query also returns same records 
+
+-- using where clause
+select users.id, users.name, bookings.booking_status 
+from users
+cross join bookings 
+where users.id = bookings.customer_id; 
+
+-- using on 
+select users.id, users.name, bookings.booking_status 
+from users
+cross join bookings 
+on users.id = bookings.customer_id; 
+-- above query work same as inner join 
+
+-- 6.SELF JOIN 
+-- In this join table is joined with itself
+-- syntax - 
+-- select column_names
+-- from table1 t1, table1 t2
+-- where condition;
+
+-- UNION Operator 
+-- it is used to combine results of tow or more select statements
+-- every select statements within UNION must have same numbers of columns 
+-- columns must have similar data types 
+-- the order of columns is also have to same 
+-- syntax - 
+-- select columns from table1
+-- union 
+-- select columns from table2;
+
+-- UNION ALL 
+-- union select distinct values by default. to allow duplicate values use UNION ALL.
+-- syntax - 
+-- select columns from table1
+-- UNION ALL
+-- select columns from table2;
+select users.id, users.name, bookings.booking_status
+from users
+left join bookings 
+on users.id = bookings.customer_id
+union all 
+select users.id, users.name, bookings.booking_status
+from users
+right join bookings 
 on users.id = bookings.customer_id;
+
+select * from bookings;
+select * from users;
+-- Basic JOIN Questions
+-- 1.List all bookings with the customer’s name.
+select users.name, bookings.* 
+from users 
+right join bookings
+on users.id = bookings.customer_id;
+
+-- 2.Show each booking with both customer and driver names.
+select a.name customer_name, c.name driver_name, b.* 
+from bookings b
+join users a
+on a.id = b.customer_id
+left join users c on c.id = b.driver_id;
+
+-- 3.List the names and phone numbers of all drivers who have completed at least one booking.
+select users.name, users.phone 
+from users
+join bookings 
+on users.id = bookings.driver_id
+where bookings.booking_status = 'Completed';
+
+-- 4.Get all customer names who have made a booking with a fare above ₹300.
+select users.name, b.fare 
+from users
+join bookings b 
+on users.id = b.customer_id
+where b.fare > 300;
+
+-- 5.Find the pickup and dropoff locations for each booking along with the customer’s and driver’s email.
+select a.email as customer_email, c.email as driver_emial, b.pickup_location, b.dropoff_location 
+from bookings b
+join users a 
+on a.id = b.customer_id
+join users c on c.id = b.driver_id;
+
+-- Aggregated JOIN Questions
+-- 1.Show the total number of bookings made by each customer.
+
+-- 2.List drivers and the total fare they earned from completed bookings.
+-- 3.Find the average fare per driver.
+-- 4.List customers who made more than 3 bookings.
+-- 5.Get the total fare paid by each customer.
+
+create table customers(
+ customer_id int primary key auto_increment,
+ name varchar(50) not null,
+ city varchar(50) not null
+);
+desc customers;
+INSERT INTO customers (customer_id, name, city) VALUES
+(1, 'Alice', 'New York'),
+(2, 'Bob', 'Chicago'),
+(3, 'Charlie', 'Boston'),
+(4, 'Diana', 'Seattle'),
+(5, 'Edward', 'Miami');
+drop table orders;
+create table orders(
+	order_id int primary key auto_increment,
+    customer_id int not null,
+    order_date date not null,
+    amount decimal not null,
+    foreign key(customer_id) references customers(customer_id)
+);
+desc orders;
+alter table orders modify customer_id int null;
+INSERT INTO orders (order_id, customer_id, order_date, amount) VALUES
+(101, 1, '2024-11-01', 250),
+(102, 3, '2024-11-05', 450),
+(103, 1, '2024-11-07', 300),
+(104, 2, '2024-11-10', 150),
+(105, NULL, '2024-11-15', 500);
+ 
+ select * from customers;
+ select * from orders;
+ 
+ select c.*, o.* 
+ from customers c
+ left join orders o
+ on o.customer_id = c.customer_id
+ union
+ select c.*, o.* 
+ from customers c
+ right join orders o
+ on o.customer_id = c.customer_id;
+ 
+ select c.name, o.order_id, o.amount 
+ from orders o
+ left join customers c 
+ on o.customer_id = c.customer_id;
+ 
+ 
+ select c.*, o.*
+ from customers c
+ left join orders o
+ on c.customer_id = o.customer_id;
+ 
+ select c.name as customer_name, o.order_id, o.order_date, o.amount
+ from orders o
+ join customers c 
+ on o.customer_id = c.customer_id;
+ -- 2.Show all customers, even if they haven't placed an order
+ 
+ 
+
